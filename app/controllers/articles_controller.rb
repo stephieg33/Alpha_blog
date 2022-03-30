626be68aa4c2da79c0a53889359313897914debf
Stephen Gash <stephieg33@gmail.com>
2022-03-30 11:40:31 +0000

    added edit and update functions to Articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,40 @@
 class ArticlesController < ApplicationController
+    
     def show
         @article = Article.find(params[:id])
     end
+    
     def index
         @articles = Article.all
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def edit
+         @article = Article.find(params[:id])
+    end
+    
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        
+    
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+            if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was successfully updated"
+            redirect_to @article
+            else  
+            render 'edit'
+            end
     end
 end
