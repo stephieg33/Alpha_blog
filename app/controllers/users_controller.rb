f1e9b25b74c2a6c884be987d97a99d6040227576
Stephen Gash <stephieg33@gmail.com>
2022-04-25 14:41:58 +0000

    added sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha-blog#{@user.username}, you have successfully created an account"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+  
+  def update
+  end
+  
+  private
+  
+   def user_params
+     params.require(:user).permit(:username, :email, :password)
+   end
+    
+end
